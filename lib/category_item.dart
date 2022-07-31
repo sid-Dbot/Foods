5b2158638d5fa452b0d7311c29a03683e615b290
sid <s.sidd707@gmail.com>
2022-07-31 20:12:35 +0545

    Navigation added

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_screen.dart';
 
 class CategortItem extends StatelessWidget {
   final String title;
@@ -6,29 +7,42 @@ class CategortItem extends StatelessWidget {
 
   CategortItem(this.title, this.colors);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoryScreen();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      child: Text(
-        title,
-        style: TextStyle(
-          fontFamily: 'RobotoCondensed',
-          fontSize: 25,
-          fontStyle: FontStyle.italic,
-          color: Color.fromRGBO(25, 25, 25, 0.7),
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(8),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 25,
+            fontStyle: FontStyle.italic,
+            color: Color.fromRGBO(25, 25, 25, 0.7),
+          ),
         ),
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            colors.withOpacity(0.4),
-            colors,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              colors.withOpacity(0.4),
+              colors,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(16),
         ),
-        borderRadius: BorderRadius.circular(16),
       ),
     );
   }
---
+++ lib/category_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New page'),
+      ),
+      body: Center(child: Text('PAGE')),
+    );
+  }
+}
