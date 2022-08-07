fc50a7b986f6d8a88abb15b09735c5d9a818e830
sid <s.sidd707@gmail.com>
2022-08-07 19:00:59 +0545

    naming

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -7,7 +7,7 @@ class CategoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('New page'),
+        title: Text('$title Foods'),
       ),
       body: Center(child: Text("This the page displays $title food items")),
     );
